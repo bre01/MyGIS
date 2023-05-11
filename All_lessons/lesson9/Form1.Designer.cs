34367821cee3f2b257e9a33eacf02f9de42aafbd
bre <iwanttosignup321@gmail.com>
2023-05-11 10:20:12 +0800

    copy code from lesson8 and change namespace

--- All_lessons/lesson9/Form1.Designer.cs
+++ All_lessons/lesson9/Form1.Designer.cs
@@ -28,13 +28,249 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.x_extent_box = new System.Windows.Forms.TextBox();
+            this.shape_box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.y_extent_box = new System.Windows.Forms.TextBox();
+            this.lab = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 36);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(57, 72);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Show Map";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(344, 36);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Zoom in";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.map_button_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(452, 36);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 28);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Zoom out";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.map_button_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(560, 36);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 28);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Move up";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.map_button_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(668, 36);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 28);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Move down";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.map_button_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(776, 36);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 28);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Move left";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.map_button_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(884, 36);
+            this.button8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 28);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Move right";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.map_button_Click);
+            // 
+            // x_extent_box
+            // 
+            this.x_extent_box.Location = new System.Drawing.Point(73, 128);
+            this.x_extent_box.Name = "x_extent_box";
+            this.x_extent_box.Size = new System.Drawing.Size(330, 22);
+            this.x_extent_box.TabIndex = 8;
+            this.x_extent_box.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // shape_box
+            // 
+            this.shape_box.Location = new System.Drawing.Point(533, 1);
+            this.shape_box.Name = "shape_box";
+            this.shape_box.Size = new System.Drawing.Size(89, 22);
+            this.shape_box.TabIndex = 9;
+            this.shape_box.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "X extent";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(456, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "shapeKind";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(165, 72);
+            this.button9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 28);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "Attributes";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(381, 84);
+            this.button10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(63, 32);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "Save as";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(497, 84);
+            this.button11.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(98, 38);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "Open";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // y_extent_box
+            // 
+            this.y_extent_box.Location = new System.Drawing.Point(73, 175);
+            this.y_extent_box.Name = "y_extent_box";
+            this.y_extent_box.Size = new System.Drawing.Size(330, 22);
+            this.y_extent_box.TabIndex = 15;
+            // 
+            // lab
+            // 
+            this.lab.AutoSize = true;
+            this.lab.Location = new System.Drawing.Point(9, 181);
+            this.lab.Name = "lab";
+            this.lab.Size = new System.Drawing.Size(54, 16);
+            this.lab.TabIndex = 16;
+            this.lab.Text = "Y extent";
+            this.lab.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.lab);
+            this.Controls.Add(this.y_extent_box);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shape_box);
+            this.Controls.Add(this.x_extent_box);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "                                                            ";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox x_extent_box;
+        private System.Windows.Forms.TextBox shape_box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.TextBox y_extent_box;
+        private System.Windows.Forms.Label lab;
     }
 }
 

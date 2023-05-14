fc5c34ae5efa67cc0c3ff2740ea38c8ab04db142
Tessa <64588494+bre01@users.noreply.github.com>
2023-05-14 23:48:42 +0800

    copy from lesson9 and update solution file

---
+++ All_lessons/lesson11/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace lesson11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.x_extent_box = new System.Windows.Forms.TextBox();
+            this.shape_box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.y_extent_box = new System.Windows.Forms.TextBox();
+            this.lab = new System.Windows.Forms.Label();
+            this.button12 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(43, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Show Map";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(258, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Zoom in";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.map_button_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(339, 29);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Zoom out";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.map_button_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(420, 29);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Move up";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.map_button_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(501, 29);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Move down";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.map_button_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(582, 29);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Move left";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.map_button_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(663, 29);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Move right";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.map_button_Click);
+            // 
+            // x_extent_box
+            // 
+            this.x_extent_box.Location = new System.Drawing.Point(55, 104);
+            this.x_extent_box.Margin = new System.Windows.Forms.Padding(2);
+            this.x_extent_box.Name = "x_extent_box";
+            this.x_extent_box.Size = new System.Drawing.Size(248, 20);
+            this.x_extent_box.TabIndex = 8;
+            this.x_extent_box.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // shape_box
+            // 
+            this.shape_box.Location = new System.Drawing.Point(400, 1);
+            this.shape_box.Margin = new System.Windows.Forms.Padding(2);
+            this.shape_box.Name = "shape_box";
+            this.shape_box.Size = new System.Drawing.Size(68, 20);
+            this.shape_box.TabIndex = 9;
+            this.shape_box.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 106);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "X extent";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(342, 3);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "shapeKind";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(124, 58);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "Attributes";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(286, 68);
+            this.button10.Margin = new System.Windows.Forms.Padding(2);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(47, 26);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "Save as";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(373, 68);
+            this.button11.Margin = new System.Windows.Forms.Padding(2);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(74, 31);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "Open";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // y_extent_box
+            // 
+            this.y_extent_box.Location = new System.Drawing.Point(55, 142);
+            this.y_extent_box.Margin = new System.Windows.Forms.Padding(2);
+            this.y_extent_box.Name = "y_extent_box";
+            this.y_extent_box.Size = new System.Drawing.Size(248, 20);
+            this.y_extent_box.TabIndex = 15;
+            // 
+            // lab
+            // 
+            this.lab.AutoSize = true;
+            this.lab.Location = new System.Drawing.Point(7, 147);
+            this.lab.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lab.Name = "lab";
+            this.lab.Size = new System.Drawing.Size(46, 13);
+            this.lab.TabIndex = 16;
+            this.lab.Text = "Y extent";
+            this.lab.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(10, 187);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 17;
+            this.button12.Text = "Clear Selection";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 18;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.lab);
+            this.Controls.Add(this.y_extent_box);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shape_box);
+            this.Controls.Add(this.x_extent_box);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "                                                            ";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox x_extent_box;
+        private System.Windows.Forms.TextBox shape_box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.TextBox y_extent_box;
+        private System.Windows.Forms.Label lab;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ All_lessons/lesson11/Form1.cs
@@ -0,0 +1,253 @@
+﻿using My.GIS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace lesson11
+{
+    public partial class Form1 : Form
+    {
+        Layer _layer = null;
+        MapAndClientConverter _converter = null;
+        Form2 _attributeWindow = null;
+        Bitmap _backWindow;
+        public Form1()
+        {
+            InitializeComponent();
+            _converter = new MapAndClientConverter(new GISMapExtent(new GISVertex(0, 0), new GISVertex(100, 100)), ClientRectangle);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Shapefile file |*.shp";
+            openFileDialog.RestoreDirectory = false;
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+            ShapefileTools shapefileTools = new ShapefileTools();
+            _layer = shapefileTools.ReadShapefile(openFileDialog.FileName);
+            _layer.DrawAttributeOrNot = false;
+            MessageBox.Show("Read " + _layer.FeatureCount() + " objects");
+            UpdateAndDraw();
+            shape_box.Text = _layer.ShapeType.ToString();
+            x_extent_box.Text = String.Format("Min:" + "{0:0.000}" + " Max:" + "{1:0.00}", _layer.Extent.minX(), _layer.Extent.maxX());
+            y_extent_box.Text = String.Format("Min:" + "{0:0.000}" + " Max:" + "{1:0.00}", _layer.Extent.minY(), _layer.Extent.maxY());
+
+            /*_converter.UpdateConverter(_layer.Extent,ClientRectangle);
+            DrawMap();*/
+
+            /*ShapefileTools shapfileTools = new ShapefileTools();
+            _layer = shapfileTools.ReadShapefile(@"C:\Users\bre\Downloads\LANDrop\GisData\chap10\thermal.shp");
+            string fileName = null;
+            _layer.DrawAttributeOrNot = false;
+            MessageBox.Show("Fount" + _layer.FeatureCount() + "Points" + fileName);*/
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //update map button clicked
+            /*_converter.UpdateConverter(_layer.Extent, ClientRectangle);
+            DrawMap();*/
+            UpdateAndDraw();
+        }
+        public void UpdateAndDraw()
+        {
+
+            _converter.UpdateConverter(_layer.Extent, this.ClientRectangle);
+            DrawMap();
+            UpdateStatusBar();
+        }
+        void UpdateStatusBar()
+        {
+            toolStripStatusLabel1.Text = _layer.Selection.Count.ToString();
+        }
+
+
+        private void DrawMap()
+        {
+            if (ClientRectangle.Width * ClientRectangle.Height == 0)
+            {
+                return;
+            }
+            _converter.UpdateConverter(_layer.Extent, this.ClientRectangle);
+            if (_backWindow != null)
+            {
+                _backWindow.Dispose();
+            }
+            _backWindow = new Bitmap(ClientRectangle.Width, ClientRectangle.Height);
+            Graphics graphics = Graphics.FromImage(_backWindow);
+
+            //Graphics graphics = CreateGraphics();
+            graphics.FillRectangle(new SolidBrush(Color.Black), ClientRectangle);
+            _layer.Draw(graphics, _converter);
+            Graphics graphics1 = CreateGraphics();
+            graphics1.DrawImage(_backWindow, 0, 0);
+
+        }
+        private void map_button_Click(object sender, EventArgs e)
+        {
+            GISMapActions mapActions = GISMapActions.zoomin;
+            if ((Button)sender == button3)
+            {
+                mapActions = GISMapActions.zoomin;
+            }
+            else if ((Button)sender == button4)
+            {
+                mapActions = GISMapActions.zoomout;
+            }
+            else if ((Button)sender == button5)
+            {
+                mapActions = GISMapActions.moveViewDown;
+            }
+            else if ((Button)sender == button6)
+            {
+                mapActions = GISMapActions.moveViewUp;
+            }
+            else if ((Button)sender == button7)
+            {
+                mapActions = GISMapActions.moveViewRight;
+            }
+            else if ((Button)sender == button8)
+            {
+                mapActions = GISMapActions.moveViewLeft;
+            }
+            _converter.ChangeView(mapActions);
+            this.DrawMap();
+
+
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (_layer != null)
+                UpdateAndDraw();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //Form2 form2 = new Form2(_layer);
+            //form2.Show();
+            OpenAttributeWindow();
+
+        }
+        private void OpenAttributeWindow()
+        {
+            if (_layer == null) return;
+            if (_attributeWindow == null) _attributeWindow = new Form2(_layer, this);
+            if (_attributeWindow.IsDisposed) _attributeWindow = new Form2(_layer, this);
+            _attributeWindow.Show();
+            if (_attributeWindow.WindowState == FormWindowState.Minimized) _attributeWindow.WindowState = FormWindowState.Normal;
+            _attributeWindow.BringToFront();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            if (dialog.ShowDialog() != DialogResult.OK) return;
+            string fileName = dialog.FileName;
+            MyFiles.WriteFile(_layer, fileName);
+            MessageBox.Show("done");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() != DialogResult.OK) return;
+            string fileName = dialog.FileName;
+            _layer = MyFiles.ReadFile(fileName);
+            MessageBox.Show("Read " + _layer.FeatureCount() + " objects");
+            _converter.UpdateConverter(_layer.Extent, ClientRectangle);
+            DrawMap();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            if (_layer == null) return;
+            GISVertex vertex = _converter.ToMapVertex(new Point(e.X, e.Y));
+            //GISSelect gs = new GISSelect();
+            //if (gs.Select(vertex, _layer.GetAllFeatures(), _layer.ShapeType, _converter) == SelectResult.Ok)
+            //{
+            //    if (_layer.ShapeType == S.Polygon)
+            //        MessageBox.Show(gs.SelectedFeatures[0].GetAttribute(0).ToString());
+            //    else
+            //        MessageBox.Show(gs.SelectedFeature.GetAttribute(0).ToString());
+            //}
+            SelectResult sr = _layer.Select(vertex, _converter);
+            if (sr == SelectResult.Ok)
+            {
+                UpdateAndDraw();
+                toolStripStatusLabel1.Text = _layer.Selection.Count.ToString();
+                UpdateAttributeWindow();
+            }
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (_layer == null) return;
+            _layer.ClearSelection();
+            UpdateAndDraw();
+            toolStripStatusLabel1.Text = "0";
+            UpdateAttributeWindow();
+        }
+        private void UpdateAttributeWindow()
+        {
+            if (_layer == null) return;
+            if (_attributeWindow == null) return;
+            if (_attributeWindow.IsDisposed) return;
+            _attributeWindow.UpdateData();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (_backWindow != null)
+            {
+                e.Graphics.DrawImage(_backWindow, 0, 0);
+            }
+        }
+    }
+
+
+}
